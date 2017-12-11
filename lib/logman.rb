e606ab2606923f17dc2119c02f368c4b2c6a62db
Igor Šarčević <igor@renderedtext.com>
2017-12-11 10:09:42 +0000

    Log metadata

--- lib/logman.rb
+++ lib/logman.rb
@@ -9,24 +9,24 @@ module Logman
     "#{Logman.format(event)}\n"
   end
 
-  def self.fatal(message)
-    DEFAULT_LOGGER.fatal(:event => message)
+  def self.fatal(message, metadata = {})
+    DEFAULT_LOGGER.fatal({:event => message}.merge(metadata))
   end
 
-  def self.error(message)
-    DEFAULT_LOGGER.error(:event => message)
+  def self.error(message, metadata = {})
+    DEFAULT_LOGGER.error({:event => message}.merge(metadata))
   end
 
-  def self.warn(message)
-    DEFAULT_LOGGER.warn(:event => message)
+  def self.warn(message, metadata = {})
+    DEFAULT_LOGGER.warn({:event => message}.merge(metadata))
   end
 
-  def self.info(message)
-    DEFAULT_LOGGER.info(:event => message)
+  def self.info(message, metadata = {})
+    DEFAULT_LOGGER.info({:event => message}.merge(metadata))
   end
 
-  def self.debug(message)
-    DEFAULT_LOGGER.debug(:event => message)
+  def self.debug(message, metadata = {})
+    DEFAULT_LOGGER.debug({:event => message}.merge(metadata))
   end
 
   def self.format(event_hash)
--- spec/logman_spec.rb
+++ spec/logman_spec.rb
@@ -12,41 +12,51 @@ RSpec.describe Logman do
 
   describe ".fatal" do
     it "displays a fatal message to STDOUT" do
-      msg = "level='F' time='2017-12-11 09:47:27 +0000' event='Hello World'\n"
+      msg = "level='F' time='2017-12-11 09:47:27 +0000' event='Hello World' from='shiroyasha'\n"
 
-      expect { Logman.fatal("Hello World") }.to output(msg).to_stdout_from_any_process
+      expect {
+        Logman.fatal("Hello World", :from => "shiroyasha")
+      }.to output(msg).to_stdout_from_any_process
     end
   end
 
   describe ".error" do
     it "displays an error message to STDOUT" do
-      msg = "level='E' time='2017-12-11 09:47:27 +0000' event='Hello World'\n"
+      msg = "level='E' time='2017-12-11 09:47:27 +0000' event='Hello World' from='shiroyasha'\n"
 
-      expect { Logman.error("Hello World") }.to output(msg).to_stdout_from_any_process
+      expect {
+        Logman.error("Hello World", :from => "shiroyasha")
+      }.to output(msg).to_stdout_from_any_process
     end
   end
 
   describe ".warn" do
     it "displays an warning message to STDOUT" do
-      msg = "level='W' time='2017-12-11 09:47:27 +0000' event='Hello World'\n"
+      msg = "level='W' time='2017-12-11 09:47:27 +0000' event='Hello World' from='shiroyasha'\n"
 
-      expect { Logman.warn("Hello World") }.to output(msg).to_stdout_from_any_process
+      expect {
+        Logman.warn("Hello World", :from => "shiroyasha")
+      }.to output(msg).to_stdout_from_any_process
     end
   end
 
   describe ".info" do
     it "displays an info message to STDOUT" do
-      msg = "level='I' time='2017-12-11 09:47:27 +0000' event='Hello World'\n"
+      msg = "level='I' time='2017-12-11 09:47:27 +0000' event='Hello World' from='shiroyasha'\n"
 
-      expect { Logman.info("Hello World") }.to output(msg).to_stdout_from_any_process
+      expect {
+        Logman.info("Hello World", :from => "shiroyasha")
+      }.to output(msg).to_stdout_from_any_process
     end
   end
 
   describe ".debug" do
     it "displays a debug message to STDOUT" do
-      msg = "level='D' time='2017-12-11 09:47:27 +0000' event='Hello World'\n"
+      msg = "level='D' time='2017-12-11 09:47:27 +0000' event='Hello World' from='shiroyasha'\n"
 
-      expect { Logman.debug("Hello World") }.to output(msg).to_stdout_from_any_process
+      expect {
+        Logman.debug("Hello World", :from => "shiroyasha")
+      }.to output(msg).to_stdout_from_any_process
     end
   end
 end
